38d33410d6ce6a72372a6865639d964d24852295
JamesWrigley <james@puiterwijk.org>
2026-02-28 11:34:49 +0100

    Apply runic formatting, and replace CompatHelper with dependabot

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,5 +9,5 @@ if GROUP == :All
     # Run all tests except those tagged with :nopre
     @run_package_tests filter = ti -> !(:nopre in ti.tags)
 else
-    @run_package_tests verbose=true filter = ti -> (GROUP in ti.tags)
+    @run_package_tests verbose = true filter = ti -> (GROUP in ti.tags)
 end
